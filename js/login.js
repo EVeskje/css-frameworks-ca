b227d9aa91d80039afe4072e56d91f54c3cad619
Eirik Nordstrand Veskje <eirik.nv@hotmail.com>
2023-11-17 16:09:44 +0100

    Update hotjar

--- js/login.js
+++ js/login.js
@@ -7,8 +7,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Check if the username and password have the correct length
   if (username.length >= 3 && password.length >= 6) {
-   // Redirect to the feed page (replace "/feed" with the actual URL)
-   window.location.href = "/feed";
+   // Redirect to the feed page
+   window.location.href = "/feed/index.html";
   } else {
    // Handle invalid username or password length
    alert("Invalid username or password length. Please try again.");
