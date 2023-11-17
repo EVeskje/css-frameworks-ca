8056a8399008bf33a7973318a69d876315e8547b
Eirik Nordstrand Veskje <eirik.nv@hotmail.com>
2023-11-17 15:59:06 +0100

    Added js login

---
+++ js/login.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+ // Add an event listener to the login button
+ document.getElementById("loginButton").addEventListener("click", function () {
+  // Get the username and password input values
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  // Check if the username and password have the correct length
+  if (username.length >= 3 && password.length >= 6) {
+   // Redirect to the feed page (replace "/feed" with the actual URL)
+   window.location.href = "/feed";
+  } else {
+   // Handle invalid username or password length
+   alert("Invalid username or password length. Please try again.");
+  }
+ });
+});
