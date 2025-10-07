2c7a2706e8bdc9918a57496de944231f87fb00b1
Eirik Nordstrand Veskje <eirik.nv@hotmail.com>
2025-10-07 20:50:49 +0200

    Vite fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  server: { port: 5173, open: true },
-  build: { outDir: "dist" }
+ server: { port: 5173, open: true },
+ build: {
+  outDir: "dist",
+  rollupOptions: {
+   input: {
+    main: resolve(__dirname, "index.html"),
+    feed: resolve(__dirname, "feed/index.html"),
+    profile: resolve(__dirname, "profile/index.html"),
+   },
+  },
+ },
 });
